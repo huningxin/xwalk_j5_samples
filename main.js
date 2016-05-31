7775a3185ee021087b2b522507ef366479521247
Ningxin Hu <ningxin.hu@intel.com>
2016-05-31 13:47:22 +0800

    Add LCD example

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ module.paths.push(process.cwd() + '/node_modules');
 
 var five = require('johnny-five');
 var board = new five.Board({repl: false});
-var led, button, rotary;
+var led, button, rotary, lcd;
 
 board.on('ready', function() {
   console.log('Johnny-Five is ready');
@@ -49,7 +49,7 @@ board.on('ready', function() {
     led.stop().off();
   };
   var ledBrightnessSlider = document.getElementById('led-brightness');
-  ledBrightnessSlider.onchange = function(e) {
+  ledBrightnessSlider.oninput = function(e) {
     led.brightness(e.target.value);
   }
 
@@ -86,4 +86,74 @@ board.on('ready', function() {
   rotary.scale(0, 255).on("change", function() {
     rotaryMeter.value = this.value.toFixed();
   });
+
+  // Plug the LCD module into any of the
+  // Grove Shield's I2C jacks.
+  lcd = new five.LCD({
+    controller: "JHD1313M1"
+  });
+
+  var lcdId = document.getElementById('lcd-id');
+  var lcdController = document.getElementById('lcd-controller');
+  lcdId.innerHTML = lcd.id;
+  lcdController.innerHTML = lcd.controller;
+
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  function rgbToString(r, g, b) {
+    return  'rgb(' + r + ', ' + g + ', ' + b + ')';
+  }
+
+  // https://crosswalk-project.org/jira/browse/XWALK-6958
+  /*
+  var lcdColorPicker = document.getElementById('lcd-color');
+  lcdColorPicker.onchange = function(e) {
+    var rgb = hexToRgb(e.target.value);
+    lcd.bgColor(rgb.r, rgb.g, rgb.b);
+  };
+  */
+
+  var lcdRgb = document.getElementById('lcd-rgb');
+  var r = 128, g = 128, b = 128;
+  lcd.bgColor(r, g, b);
+  lcdRgb.style.backgroundColor = rgbToString(r, g, b);
+
+  var lcdRedSlider = document.getElementById('lcd-red');
+  lcdRedSlider.oninput = function(e) {
+    r = e.target.value
+    lcdRgb.style.backgroundColor = rgbToString(r, g, b);
+    lcd.bgColor(r, g, b);
+  }
+
+  var lcdGreenSlider = document.getElementById('lcd-green');
+  lcdGreenSlider.oninput = function(e) {
+    g = e.target.value
+    lcdRgb.style.backgroundColor = rgbToString(r, g, b);
+    lcd.bgColor(r, g, b);
+  }
+
+  var lcdBlueSlider = document.getElementById('lcd-blue');
+  lcdBlueSlider.oninput = function(e) {
+    b = e.target.value
+    lcdRgb.style.backgroundColor = rgbToString(r, g, b);
+    lcd.bgColor(r, g, b);
+  }
+
+  var lcdText = document.getElementById('lcd-text');
+  lcdText.onkeyup = function(e) {
+    lcd.clear();
+    var text = e.target.value;
+    var line1 = text.slice(0, 16);
+    var line2 = text.slice(16);
+    lcd.cursor(0, 0).print(line1);
+    lcd.cursor(1, 0).print(line2);
+  }
+
 });
