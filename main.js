c11e36dec4852c216f3d3407ee96ae392bee4a32
Ningxin Hu <ningxin.hu@intel.com>
2016-06-12 16:31:04 +0800

    Add camera preview

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ if (typeof process !== 'undefined') {
     InitUI(control);
 
     InitServer(control);
+
+    InitCamera();
   });
 
   function CreateControl(board) {
@@ -78,6 +80,26 @@ if (typeof process !== 'undefined') {
     }));
 
     return control;
+  }
+
+  function InitCamera() {
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    var constraints = {video: true};
+
+    function successCallback(localMediaStream) {
+      window.stream = localMediaStream; // stream available to console
+      var video = document.querySelector("video");
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    }
+
+    function errorCallback(error){
+      console.log("navigator.getUserMedia error: ", error);
+    }
+
+    navigator.getUserMedia(constraints, successCallback, errorCallback);
   }
 } else {
   window.onload = function(e) {
